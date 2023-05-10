e0fef2dfc2cc3c0ed23f17f40923f51177cb0452
zZENNn <arseniy.nefyodov@gmail.com>
2023-05-11 01:37:56 +0700

    Добавил валидацию

--- script.js
+++ script.js
@@ -1,11 +1,48 @@
 const form = document.getElementById("form")
-console.log(form)
+const timeAlert = document.getElementById("timeAlert")
+const startTime = document.getElementById("startTime")
+
+const endTime = document.getElementById("endTime")
+// console.log(form)
+
 form.onsubmit = (event)=>{
     event.preventDefault()
+    let inputs = document.getElementsByClassName("field")
+    // console.log(inputs)
+    let validity = true
+    for(let i = 0;i<inputs.length;i++){
+            if(inputs[i].value == ""){
+                inputs[i].classList.add("invalid")
+                validity = false
+            }
+            else{
+                inputs[i].classList.remove("invalid")
+                
+                
+            }
+           
+        
+    }
+    if(startTime.value>endTime.value&&startTime.value!=""&&endTime.value!=""){
+        startTime.classList.add("invalid")
+        endTime.classList.add("invalid")
+        timeAlert.style.display = "block"
+        validity = false
+    }
+    else{
+        timeAlert.style.display = "none"
+    }
+    // console.log(startTime.value)
+    (validity===true)&&sendForm()
+}
+
+function sendForm(){
     let formData = new FormData(form)
     let formDataObject = Object.fromEntries(formData.entries());
     let formDataJsonString = JSON.stringify(formDataObject);
-  console.log(formDataJsonString)
-  form.reset()
+    console.log(formDataJsonString)
+    form.reset()
+   
+  
 }
 
